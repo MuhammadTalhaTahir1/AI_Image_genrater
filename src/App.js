d2d9a8a112ee5dfca02e2cb586e4bd9f77a34f9d
Muhammad Talha <talhatahir596@gmail.com>
2024-07-27 00:11:03 -0700

    Done

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import ImageGenrator from "./Components/ImageGenrater/ImageGenrator";
+
+function App() {
+  return (
+    <div>
+      <ImageGenrator />
+    </div>
+  );
+}
+
+export default App;
